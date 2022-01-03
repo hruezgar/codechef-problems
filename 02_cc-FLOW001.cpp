528d9b45dbbfa060a4bea2b6e15601701eba8d96
hasofn <hasanruzweb@gmail.com>
2022-01-03 10:22:28 +0300

    second problem on codechef

---
+++ 02_cc-FLOW001.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int len;
+	int num1[len];
+	int num2[len];
+	cin >> len;
+	for (int i = 0; i < len; i++){
+		cin >> num1[i] >> num2[i];
+	}
+
+	for (int i = 0; i < len; i++){
+		cout << (num1[i] + num2[i]) << endl;
+	}
+
+}
